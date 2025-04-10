de2c4869a50f4cc95c14c069b30a1f9e72b70913
Bhagyashri1102 <bhagyashrimohite1102@gmail.com>
2025-04-11 04:31:56 +0530

    main

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -43,4 +43,39 @@ router.get('/get',async(req,res)=>{
         })
     }
 })
+
+router.delete('/delete/:id',async(req,res)=>{
+    try{
+        const id=req.params.id
+        await product.findByIdAndDelete(id)
+        res.json({
+            status:true,
+            message:'product deleted successfully!!'
+          })
+    }
+    catch(err){
+        res.json({
+            status:false,
+            message:err
+        })
+    }
+})
+
+router.put('/update/:id',async(req,res)=>{
+    const id=req.params.id
+    await product.findByIdAndUpdate(id,req.body,{'new':true})
+    try{
+       const updated=await product.findByIdAndUpdate(id)
+        res.json({
+            status:true,
+            message:'product updated successfully!!'
+          })
+    }
+    catch(err){
+        res.json({
+            status:false,
+            message:err
+        }) 
+    }
+})
 module.exports=router
